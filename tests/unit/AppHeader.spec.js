99501c450f186910fa79e67980656a9894356d46
Dan Vega <danvega@gmail.com>
2019-09-28 12:59:02 -0400

    Replacing shallowMount with mount for simplicity in the article

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,14 +1,14 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import AppHeader from "@/components/AppHeader";
 
 describe("AppHeader", () => {
   test("if logged in is false do not show logout button", () => {
-    const wrapper = shallowMount(AppHeader);
+    const wrapper = mount(AppHeader);
     expect(wrapper.find("button").isVisible()).toBe(false);
   });
 
   test("if logged in show logout button", () => {
-    const wrapper = shallowMount(AppHeader);
+    const wrapper = mount(AppHeader);
     wrapper.setData({ loggedIn: true });
     expect(wrapper.find("button").isVisible()).toBe(true);
   });
--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -1,14 +1,14 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import RandomNumber from "@/components/RandomNumber";
 
 describe("RandomNumber", () => {
   test("by default the random number should be 0", () => {
-    const wrapper = shallowMount(RandomNumber);
+    const wrapper = mount(RandomNumber);
     expect(wrapper.vm.$data.randomNumber).toEqual(0);
   });
 
   test("if we click the generate button we should get a random number between 1 and 10", () => {
-    const wrapper = shallowMount(RandomNumber);
+    const wrapper = mount(RandomNumber);
     wrapper.find(".btn").trigger("click");
     const randomNumber = wrapper.vm.$data.randomNumber;
     expect(randomNumber).toBeGreaterThanOrEqual(1);
@@ -16,7 +16,7 @@ describe("RandomNumber", () => {
   });
 
   test("if we click the generate button we should get a random number between 100 and 200", () => {
-    const wrapper = shallowMount(RandomNumber, {
+    const wrapper = mount(RandomNumber, {
       propsData: {
         min: 100,
         max: 200
